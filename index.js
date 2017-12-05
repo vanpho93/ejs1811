97d977c55657a3601c8d7d871df81b955ac34418
Van Pho Nguyen <vanpho02@gmail.com>
2017-12-05 21:18:28 +0700

    xong

--- index.js
+++ index.js
@@ -12,9 +12,25 @@ const arrPerson = [
     { name: 'Teo', age: 10 },
     { name: 'Ti', age: 15 },
     { name: 'Tun', age: 20 }
-]
+];
 
 app.get('/', (req, res) => {
     res.render('home', { name: 'Pho', isAdmin: true, arrPerson });
 });
+
+app.get('/remove/:name', (req, res) => {
+    const { name } = req.params;
+    const index = arrPerson.findIndex(person => person.name === name);
+    if (index !== -1) arrPerson.splice(index, 1);
+    res.redirect('/');
+});
+
+app.get('/add/:name/:age', (req, res) => {
+    const { name, age } = req.params;
+    arrPerson.push({ name, age });
+    res.redirect('/');
+});
+
+app.get('*', (req, res) => res.redirect('/'));
+
 app.listen(3000, () => console.log('Server started!'));
