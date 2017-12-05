cbed2d6b4b4932466badc355149f23c4e53490b1
Van Pho Nguyen <vanpho02@gmail.com>
2017-12-05 20:30:55 +0700

    dev

--- index.js
+++ index.js
@@ -8,5 +8,13 @@ app.set('view engine', 'ejs');
 // app.set('views', './views');
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.render('home'));
+const arrPerson = [
+    { name: 'Teo', age: 10 },
+    { name: 'Ti', age: 15 },
+    { name: 'Tun', age: 20 }
+]
+
+app.get('/', (req, res) => {
+    res.render('home', { name: 'Pho', isAdmin: true, arrPerson });
+});
 app.listen(3000, () => console.log('Server started!'));
